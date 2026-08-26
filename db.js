2226cafb2e750f75b3e18b03e45a416beda2bf47
Bishal Bhusal <bishaldevloper6@gmail.com>
2026-08-26 19:51:28 +0545

    data base connection

--- db.js
+++ db.js
@@ -1,15 +1,20 @@
-const mongoose= require('mongoose');
-const URL="mongodb+srv://shotgaming124_db_user:<shotgaming124_db_use>@cluster0.z4c2bff.mongodb.net/"
+
 require('dotenv').config();
+const dns= require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+const mongoose = require('mongoose');
+
+const URL = process.env.MONGO_URI;
+
+console.log("Mongo URI exists:", !!URL);
+
 mongoose.connect(URL)
-const db= mongoose.connection;
-db.on('connected',()=>{
-    console.log('Mongodb connected sucessfully')
-})
-db.on('error',(err)=>{
-    console.log('Server is not coonected',err)
-})
-db.on('disconnected',()=>{
-    console.log('Server Disconnected')
-})
-module.exports=db;
+    .then(() => {
+        console.log("MongoDB connected successfully");
+    })
+    .catch((err) => {
+        console.log("MongoDB connection error:", err.message);
+    });
+
+module.exports = mongoose.connection;
